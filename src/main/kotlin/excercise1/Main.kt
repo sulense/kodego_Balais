a3b1cea305e034f18ef1e7a51f5b2497318d5651
JunReeey <balaisjohnrey80@gmail.com>
2022-10-07 20:46:10 +0800

    excercise 1 application for employment
    variable declaration

--- src/main/kotlin/excercise1/Main.kt
+++ src/main/kotlin/excercise1/Main.kt
@@ -3,56 +3,33 @@ package excercise1
 import java.time.LocalDateTime
 import java.time.Month
 import java.time.format.DateTimeFormatter
+import java.util.Date
+
 
 fun main() {
 //Application for Employment
-    var positionApplying = "Chef"
-    var desiredSalary = 10000
-    var dateAvail4work =  LocalDateTime.of(2022, Month.JANUARY, 5, 8, 10)
+    var positionApplying:String? = null
+    var desiredSalary:Int? = null
+    var dateAvail4work:Date? = null
     var dateFormat1 = DateTimeFormatter.ofPattern("M/d/y H:m:ss")
 //PERSONAL INFORMATION
-    var lastName = "Balais"
-    var firstName = "John Rey"
-    var middleName = "Medina"
-    var userAddress = "somewhere down the road"
-    var state = "N/A"
-    var Zip = "400"
-    var homePhone = "7463764786"
-    var cellPhone = "0922222222"
-    var socialSecuritySys = "657676"
-    var usCitizen = "Yes"
-    var beenConvicted = "No"
-    var ifSelectedDrugTest = "Yes"
+    var lastName:String? = null
+    var firstName:String? = null
+    var middleName:String? = null
+    var userAddress:String? = null
+    var state:String? = null
+    var Zip:Int? = null
+    var homePhone:Int? = null
+    var cellPhone:Int? = null
+    var socialSecurityNumber:Int? = null
+    var usCitizen:String? = null
+    var beenConvicted:String? = null
+    var ifSelectedDrugTest:String? = null
 //EDUCATION
-    var schoolName = "Central Elementary School"
-    var Location = "Zamboanga Santos City"
-    var yearsAttended = "2001-2022"
-    var degreeRecieved = "BSEd"
-    var Major = "Filipino"
+    var schoolName:String? = null
+    var Location:String? = null
+    var yearsAttended:Int? = null
+    var degreeRecieved:String? = null
+    var Major:String? = null
 
-    println("Application for Employment")
-    println("Position You Are Applying For: $positionApplying")
-    println("Desired Salaray: $desiredSalary")
-    println("Date Available For Work: "+ dateAvail4work.format(dateFormat1))
-
-    println("PERSONAL INFORMATION")
-    println("Last Name: $lastName")
-    println("First Name: $firstName")
-    println("Middle Name: $middleName")
-    println("Address: $userAddress")
-    println("State: $state")
-    println("Zip: $Zip")
-    println("Home Phone: $homePhone")
-    println("Cellphone: $cellPhone")
-    println("Social Security Nummer: $socialSecuritySys")
-    println("Are you a U.S. Citizen? $usCitizen")
-    println("Have you ever been convicted of a felony? $beenConvicted")
-    println("If selected for employment are you willing to submit to a pre-employment drug screening test? $ifSelectedDrugTest ")
-
-    println("EDUCATION")
-    println("School: $schoolName")
-    println("Location: $Location")
-    println("Years Attended: $yearsAttended")
-    println("Degree Recieved: $degreeRecieved")
-    println("Major: $Major")
 }
